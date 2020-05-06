78bf49a22f3d90ecc2de53a552c5feb39805fe72
Rafael Gomes <rafaelg1402@protonmail.com>
2020-05-06 10:31:17 +0100

    add bubble_sort method and logic

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,4 +1,16 @@
 def bubble_sort(sort_array)
+  done = false
+  while !done do
+    done = true
+    i = 1
+    while i < sort_array.length do
+      if sort_array[i - 1] > sort_array[i]
+        done = false
+        sort_array[i], sort_array[i - 1] = sort_array[i - 1], sort_array[i]
+      end
+      i +=1 
+    end
+  end
   puts sort_array
 end
 bubble_sort([4,3,78,2,0,2])
